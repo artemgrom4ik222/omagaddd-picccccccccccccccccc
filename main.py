31707833a49e78c50810317486655c27c20fddb7
artemgrom4ik222 <artemcheremisin2010@gmail.com>
2025-02-21 21:11:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import choice, shuffle
+from time import sleep
+from PyQt5.QtWidgets import QApplication
+
+from main_window import *
+from menu_window import *
+
+app = QApplication([])
+
+
+
+window.show
+app.exec_()
